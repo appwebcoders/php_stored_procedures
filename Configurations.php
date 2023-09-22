e139366fc116be0aa0ef50cac2bc9d78b0407ab2
AppWeb Coders <appwebcoders@gmail.com>
2023-09-22 22:54:27 +0530

    first commit

---
+++ Configurations.php
@@ -0,0 +1,7 @@
+<?php 
+   // DEVELOPEMENT SERVER DETAILS ... 
+   $DATABASE_SERVER_IP = "localhost";
+   $DATABASE_USER_NAME = "root";
+   $DATABASE_USER_PASSWORD="";
+   $DATABASE_NAME="appwebrestapi";
+?>
